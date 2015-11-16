072920b34a6967bd832f9d3585827914ddf42a8e
Scott Landes <scott.landes@crowdrise.com>
2015-11-15 23:33:20 -0800

    Fixed bug in production settings file

--- project/settings/production.py
+++ project/settings/production.py
@@ -11,7 +11,7 @@ from __future__ import absolute_import, unicode_literals
 from django.utils import six
 
 
-from .common import *  # noqa
+from .base import *  # noqa
 
 # SECRET CONFIGURATION
 # ------------------------------------------------------------------------------
