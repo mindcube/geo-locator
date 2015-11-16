f092439738b1499984954f22fa0b6cc1686678a6
Scott Landes <scott.landes@crowdrise.com>
2015-11-16 00:28:20 -0800

    Started building out test cases

--- post_install.sh
+++ post_install.sh
@@ -1,6 +1,7 @@
 echo '*** Setting up local django install'
 sudo su - postgres -c "createdb vagrant;"
 sudo su - postgres -c "psql -c \"CREATE USER vagrant WITH PASSWORD 'vagrant'\""
+sudo su - postgres -c "psql -c 'ALTER USER vagrant CREATEDB;'"
 echo "source /var/www/.virtualenv/vagrant/bin/activate" >> ~/.bashrc
 echo "cd /var/www" >> ~/.bashrc
 source /var/www/.virtualenv/vagrant/bin/activate
--- project/apps/geo_locator/tests.py
+++ project/apps/geo_locator/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.test import Client
 
-# Create your tests here.
+class SimpleTest(TestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+
+    def test_details(self):
+        # Issue a GET request.
+        response = self.client.get('/')
+
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
